09d7683ebb5c12ec44870be14876dd01858bd304
Vedang <vedanglimaye@gmail.com>
2025-01-20 19:52:37 +0530

    Tweaked the Deploy file,Added some test addresses

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -1,7 +1,8 @@
 // SPDX-License-Identifier: MIT
-//          export GOLD PRICE FEED= 0x379589227b15F1a12195D3f2d90bBc9F31f95235; // Ethereum mainnet Gold/USD
-//          export SILVER PRICE FEED= 0x214eD9Da11D2fbe465a6fc601a91E62EbEc1a0D6; // Ethereum mainnet Silver/USD
-//          export PRIVATE KEY= (whatever key we use)
+//          export SILVER PRICE FEED= 0x379589227b15F1a12195D3f2d90bBc9F31f95235; // Ethereum mainnet Silver/USD
+//          Export GOLD Test PRICE FEED = 0xC5981F461d74c46eB4b0CF3f4Ec79f025573B0Ea
+//          export GOLD PRICE FEED= 0x214eD9Da11D2fbe465a6fc601a91E62EbEc1a0D6; // Ethereum mainnet Gold/USD
+//          export PRIVATE KEY= 0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80
 
 pragma solidity ^0.8.20;
 
